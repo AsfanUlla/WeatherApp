bb8ffff363dbaea1816431694c6271f1d13d2c79
AsfanUlla <asfanulla@gmail.com>
2021-03-01 10:08:13 +0530

    xo

--- weatherapi/openweatherapi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpenweatherapiConfig(AppConfig):
-    name = 'openweatherapi'
--- weatherapi/openweatherapi/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url 
-from openweatherapi import views
- 
-urlpatterns = [
-    url(r'^weather$', views.weather_data),
-]
